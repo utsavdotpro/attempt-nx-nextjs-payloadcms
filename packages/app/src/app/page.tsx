b8a1b1b60e02d778811468a273b7d9d21bf88341
Utsav Barnwal <u.barnwal36@gmail.com>
2023-07-23 00:05:54 +0700

    replace nextjs app with utsavdotpro/starter-nextjs-tailwind-ts

---
+++ packages/app/src/app/page.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <main className="p-8">
+      Hello from <a href="https://nextjs.org/">NextJS</a> with{' '}
+      <a className="font-bold" href="https://tailwindcss.com/">
+        Tailwind
+      </a>{' '}
+      in <a href="https://typescriptlang.org/">TypeScript</a> starter template!
+    </main>
+  );
+};
+
+export default Home;
